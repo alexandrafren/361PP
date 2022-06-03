1679903a28f2c27a113b9c772264ecc09da33637
Alexandra Fren <alexandra.fren@gmail.com>
2022-06-03 13:35:59 -0400

    removing unneccesary file

--- helpers/movies.py
+++
@@ -1,2 +0,0 @@
-movies = {1: "Train to Busan", 2: "Night of the Living Dead"}
-descriptions = {1: 'While a zombie virus breaks out in South Korea, passengers struggle to survive on the train from Seoul to Busan.', 2: 'A ragtag group of Pennsylvanians barricade themselves in an old farmhouse to remain safe from a horde of flesh-eating ghouls that are ravaging the East Coast of the United States.'}
